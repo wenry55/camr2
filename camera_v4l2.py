41a08b4ba3f9b66e0194d02020c64aefce993daf
BK <bkseo@me.com>
2021-05-10 13:38:22 +0900

    Update camera_v4l2.py

--- camera_v4l2.py
+++ camera_v4l2.py
@@ -41,7 +41,7 @@ class Camera(BaseCamera):
                 yuv[4::6] = u
                 yuv[5::6] = v
                 image = Image.frombytes("YCbCr",(size_x,size_y),yuv)
-                resize_image = image.resize((640,480))
+                resize_image = image.resize((640,360))
                 resize_image.save(bio, format="jpeg")
                 yield bio.getvalue()
                 bio.seek(0)
